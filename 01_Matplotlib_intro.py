05eaf0555c5412054f1486cbe8dfc20a14c577cc
PrithvirajGhosh10 <aloghosh802@gmail.com>
2026-05-31 13:10:09 +0530

    Introduction of Matplotlib

---
+++ 01_Matplotlib_intro.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np #using numpy as numpy arrays are faster than python lists
+# print(matplotlib.__version__)
+x=np.array([2023,2024,2025,2026])
+y=np.array([15,25,30,20])
+plt.plot(x,y)
+plt.show() #to get the graph
