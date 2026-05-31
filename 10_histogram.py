3ab174688352dc947d04929c7cb83d1dfb486794
PrithvirajGhosh10 <aloghosh802@gmail.com>
2026-05-31 22:37:44 +0530

    Histogram
     Histogram = A value representation of the distribution of quotative data.
    #             They group values into bins(intervals)
    #             and counts how many fall in each range.

---
+++ 10_histogram.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Histogram = A value representation of the distribution of quantative data.
+#             They group values into bins(intervals)
+#             and counts how many fall in each range.
+
+scores= np.random.normal(loc=80,scale=10,size=100)
+scores=np.clip(scores,0,100)
+plt.hist(scores,bins=10,color="lightgreen",edgecolor="black")
+
+plt.title("Exam Scores")
+plt.xlabel("Score")
+plt.ylabel("No of students")
+
+plt.show()
